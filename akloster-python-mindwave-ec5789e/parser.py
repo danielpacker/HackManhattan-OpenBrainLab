e0e085a59c2fa48a1cafa4102912023f631be0cb
dave <dave@dave-1005HA.(none)>
2012-02-13 19:15:44 -0500

    added bluetooth socket close on exit and fixed div0 error

--- akloster-python-mindwave-ec5789e/parser.py
+++ akloster-python-mindwave-ec5789e/parser.py
@@ -29,121 +29,123 @@ in Python in the future, but for now I am satisfied with using Python only.
 
 
 class Parser:
-	def __init__(self):
-		self.parser = self.run()
-		self.parser.next()
-		self.current_vector  =[]
-		self.raw_values =  []
-		self.current_meditation = 0
-		self.current_attention= 0
-		self.current_spectrum = []
-		self.sending_data = False
-		self.state ="initializing"
-		self.raw_file = None
-		self.esense_file = None
+        def __init__(self):
+                self.parser = self.run()
+                self.parser.next()
+                self.current_vector  =[]
+                self.raw_values =  []
+                self.current_meditation = 0
+                self.current_attention= 0
+                self.current_spectrum = []
+                self.sending_data = False
+                self.state ="initializing"
+                self.raw_file = None
+                self.esense_file = None
                 self.socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )  # use Bluetooth for MindSet
                 self.socket.connect(("00:13:EF:00:3F:FC", 3)) # use Bluetooth for MindSet
-	
-		
-	def update(self):
-		bytes = self.socket.recv(1000)		# modified to read bluetooth stream for MindSet
-		for b in bytes:
-			self.parser.send(ord(b))	# Send each byte to the generator
-	def write_serial(self, string):
-		self.dongle.write(string)
-	
-	def start_raw_recording(self, file_name):
-		self.raw_file = file(file_name, "wt")
-		self.raw_start_time = time()
-	def start_esense_recording(self, file_name):
-		self.esense_file = file(file_name, "wt")
-		self.esense_start_time = time()
-	def stop_raw_recording(self):
-		if self.raw_file:
-			self.raw_file.close()
-			self.raw_file = None
-		
-	def stop_esense_recording(self):
-		if self.esense_file:
-			self.esense_file.close()
-			self.esense_file = None
-	def run(self):
-		"""
-			This generator parses one byte at a time.
-		"""
-		last = time()
-		i = 1
-		self.buffer_len = 512*3
-		times = []
-		while 1:
-			byte = yield
-			if byte== 0xaa:
-				byte = yield # This byte should be "\aa" too
-				if byte== 0xaa:
-					# packet synced by 0xaa 0xaa
-					packet_length = yield
-					packet_code = yield
-					if packet_code == 0xd4:
-						# standing by
-						self.dongle_state= "standby"
-					elif packet_code == 0xd0:
-						self.dongle_state = "connected"
-					else:
-						self.sending_data = True
-						left = packet_length-2
-						while left>0:
-							if packet_code ==0x80: # raw value
-								row_length = yield
-								a = yield
-								b = yield
-								value = struct.unpack("<h",chr(a)+chr(b))[0]
-								self.raw_values.append(value)
-								if len(self.raw_values)>self.buffer_len:
-									self.raw_values = self.raw_values[-self.buffer_len:]
-								left-=2
-								
-								if self.raw_file:
-									t = time()-self.raw_start_time
-									self.raw_file.write("%.4f,%i\n" %(t, value))
-							elif packet_code == 0x02: # Poor signal
-								a = yield
-								self.poor_signal = a
-								if a>0:
-									pass 
-								left-=1
-							elif packet_code == 0x04: # Attention (eSense)
-								a = yield
-								if a>0:
-									v = struct.unpack("b",chr(a))[0]
-									if v>0:
-										self.current_attention = v
-										if self.esense_file:
-											self.esense_file.write("%.2f,,%i\n" % (time()-self.esense_start_time, v))
-								left-=1
-							elif packet_code == 0x05: # Meditation (eSense)
-								a = yield
-								if a>0:
-									v = struct.unpack("b",chr(a))[0]
-									if v>0:
-										self.current_meditation = v
-										if self.esense_file:
-											self.esense_file.write("%.2f,%i,\n" % (time()-self.esense_start_time, v))
 
-								left-=1
-							elif packet_code == 0x83:
-									vlength = yield
-									self.current_vector = []
-									for row in range(8):
-										a = yield
-										b = yield
-										c = yield
-										value = a*255*255+b*255+c
-										self.current_vector.append(value)
-									left-=vlength
-							packet_code = yield					
-				else:
-					pass # sync failed
-			else:
-				pass # sync failed
+        def polite(self):
+                return(self.socket)
+                
+        def update(self):
+                bytes = self.socket.recv(1000)          # modified to read bluetooth stream for MindSet
+                for b in bytes:
+                        self.parser.send(ord(b))        # Send each byte to the generator
+        def write_serial(self, string):
+                self.dongle.write(string)
+        
+        def start_raw_recording(self, file_name):
+                self.raw_file = file(file_name, "wt")
+                self.raw_start_time = time()
+        def start_esense_recording(self, file_name):
+                self.esense_file = file(file_name, "wt")
+                self.esense_start_time = time()
+        def stop_raw_recording(self):
+                if self.raw_file:
+                        self.raw_file.close()
+                        self.raw_file = None
+                
+        def stop_esense_recording(self):
+                if self.esense_file:
+                        self.esense_file.close()
+                        self.esense_file = None
+        def run(self):
+                """
+                        This generator parses one byte at a time.
+                """
+                last = time()
+                i = 1
+                self.buffer_len = 512*3
+                times = []
+                while 1:
+                        byte = yield
+                        if byte== 0xaa:
+                                byte = yield # This byte should be "\aa" too
+                                if byte== 0xaa:
+                                        # packet synced by 0xaa 0xaa
+                                        packet_length = yield
+                                        packet_code = yield
+                                        if packet_code == 0xd4:
+                                                # standing by
+                                                self.dongle_state= "standby"
+                                        elif packet_code == 0xd0:
+                                                self.dongle_state = "connected"
+                                        else:
+                                                self.sending_data = True
+                                                left = packet_length-2
+                                                while left>0:
+                                                        if packet_code ==0x80: # raw value
+                                                                row_length = yield
+                                                                a = yield
+                                                                b = yield
+                                                                value = struct.unpack("<h",chr(a)+chr(b))[0]
+                                                                self.raw_values.append(value)
+                                                                if len(self.raw_values)>self.buffer_len:
+                                                                        self.raw_values = self.raw_values[-self.buffer_len:]
+                                                                left-=2
+                                                                
+                                                                if self.raw_file:
+                                                                        t = time()-self.raw_start_time
+                                                                        self.raw_file.write("%.4f,%i\n" %(t, value))
+                                                        elif packet_code == 0x02: # Poor signal
+                                                                a = yield
+                                                                self.poor_signal = a
+                                                                if a>0:
+                                                                        pass 
+                                                                left-=1
+                                                        elif packet_code == 0x04: # Attention (eSense)
+                                                                a = yield
+                                                                if a>0:
+                                                                        v = struct.unpack("b",chr(a))[0]
+                                                                        if v>0:
+                                                                                self.current_attention = v
+                                                                                if self.esense_file:
+                                                                                        self.esense_file.write("%.2f,,%i\n" % (time()-self.esense_start_time, v))
+                                                                left-=1
+                                                        elif packet_code == 0x05: # Meditation (eSense)
+                                                                a = yield
+                                                                if a>0:
+                                                                        v = struct.unpack("b",chr(a))[0]
+                                                                        if v>0:
+                                                                                self.current_meditation = v
+                                                                                if self.esense_file:
+                                                                                        self.esense_file.write("%.2f,%i,\n" % (time()-self.esense_start_time, v))
+
+                                                                left-=1
+                                                        elif packet_code == 0x83:
+                                                                        vlength = yield
+                                                                        self.current_vector = []
+                                                                        for row in range(8):
+                                                                                a = yield
+                                                                                b = yield
+                                                                                c = yield
+                                                                                value = a*255*255+b*255+c
+                                                                                self.current_vector.append(value)
+                                                                        left-=vlength
+                                                        packet_code = yield                                     
+                                else:
+                                        pass # sync failed
+                        else:
+                                pass # sync failed
 dongle_state = None
 DONGLE_STANDBY= "Standby"
--- akloster-python-mindwave-ec5789e/sdl_viewer.py
+++ akloster-python-mindwave-ec5789e/sdl_viewer.py
@@ -13,6 +13,8 @@ pygame.display.set_caption("Mindwave Viewer")
 from parser import Parser
 
 p = Parser()
+socket = p.polite()
+print socket
 
 blackColor = pygame.Color(0,0,0)
 redColor = pygame.Color(255,0,0)
@@ -38,79 +40,82 @@ attention_img = font.render("Attention", False, redColor)
 record_baseline = False
 
 while True:
-	p.update()
-	window.blit(background_img,(0,0))
-	if p.sending_data:
-		iteration+=1
-		
-		flen = 50
-			
-		if len(p.raw_values)>=500:
-			spectrum, relative_spectrum = bin_power(p.raw_values[-p.buffer_len:], range(flen),512)
-			spectra.append(array(relative_spectrum))
-			if len(spectra)>30:
-				spectra.pop(0)
-				
-			spectrum = mean(array(spectra),axis=0)
-			for i in range (flen-1):
-				value = float(spectrum[i]*1000) 
-				if i<3:
-					color = deltaColor
-				elif i<8:
-					color = thetaColor
-				elif i<13:
-					color = alphaColor
-				elif i<30:
-					color = betaColor
-				else:
-					color = gammaColor
-				pygame.draw.rect(window, color, (25+i*10,400-value, 5,value))
-		else:
-			pass
-		pygame.draw.circle(window,redColor, (800,200),p.current_attention/2)
-		pygame.draw.circle(window,greenColor, (800,200),60/2,1)
-		pygame.draw.circle(window,greenColor, (800,200),100/2,1)
-		window.blit(attention_img, (760,260))
-		pygame.draw.circle(window,redColor, (700,200),p.current_meditation/2)
-		pygame.draw.circle(window,greenColor, (700,200),60/2,1)
-		pygame.draw.circle(window,greenColor, (700,200),100/2,1)
-		
-		window.blit(meditation_img, (600,260))
-		if len(p.current_vector)>7:
-			m = max(p.current_vector)
-			for i in range(7):
-				value = p.current_vector[i] *100.0/m
-				pygame.draw.rect(window, redColor, (600+i*30,450-value, 6,value))
+        p.update()
+        window.blit(background_img,(0,0))
+        if p.sending_data:
+                iteration+=1
+                
+                flen = 50
+                        
+                if len(p.raw_values)>=500:
+                        spectrum, relative_spectrum = bin_power(p.raw_values[-p.buffer_len:], range(flen),512)
+                        spectra.append(array(relative_spectrum))
+                        if len(spectra)>30:
+                                spectra.pop(0)
+                                
+                        spectrum = mean(array(spectra),axis=0)
+                        for i in range (flen-1):
+                                value = float(spectrum[i]*1000) 
+                                if i<3:
+                                        color = deltaColor
+                                elif i<8:
+                                        color = thetaColor
+                                elif i<13:
+                                        color = alphaColor
+                                elif i<30:
+                                        color = betaColor
+                                else:
+                                        color = gammaColor
+                                pygame.draw.rect(window, color, (25+i*10,400-value, 5,value))
+                else:
+                        pass
+                pygame.draw.circle(window,redColor, (800,200),p.current_attention/2)
+                pygame.draw.circle(window,greenColor, (800,200),60/2,1)
+                pygame.draw.circle(window,greenColor, (800,200),100/2,1)
+                window.blit(attention_img, (760,260))
+                pygame.draw.circle(window,redColor, (700,200),p.current_meditation/2)
+                pygame.draw.circle(window,greenColor, (700,200),60/2,1)
+                pygame.draw.circle(window,greenColor, (700,200),100/2,1)
+                
+                window.blit(meditation_img, (600,260))
+                if len(p.current_vector)>7:
+                        m = max(p.current_vector)
+                        if m == 0:
+                                m = 0.01
+                        for i in range(7):
+                                value = p.current_vector[i] *100.0/m
+                                pygame.draw.rect(window, redColor, (600+i*30,450-value, 6,value))
 
-		if raw_eeg:
-			lv = 0
-			for i,value in enumerate(p.raw_values[-1000:]):
-				v = value/ 255.0/ 5
-				pygame.draw.line(window, redColor, (i+25,500-lv),(i+25, 500-v))
-				lv = v
-	else:
-		img = font.render("Mindwave Headset is not sending data... Press F5 to autoconnect or F6 to disconnect.", False, redColor)
-		window.blit(img,(100,100))
-	
-	for event in pygame.event.get():
-		if event.type==QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type==KEYDOWN:
-			if event.key== K_F5:
-				p.write_serial("\xc2")
-			elif event.key== K_F6:
-				p.write_serial("\xc1")
-			elif event.key==K_ESCAPE:
-				pygame.quit()
-				sys.exit()
-			elif event.key == K_F7:
-				record_baseline = True
-				p.start_raw_recording("baseline_raw.csv")
-				p.start_esense_recording("baseline_esense.csv")
-			elif event.key == K_F8:
-				record_baseline = False
-				p.stop_esense_recording()
-				p.stop_raw_recording()
-	pygame.display.update()
-	fpsClock.tick(30)
+                if raw_eeg:
+                        lv = 0
+                        for i,value in enumerate(p.raw_values[-1000:]):
+                                v = value/ 255.0/ 10
+                                pygame.draw.line(window, redColor, (i+25,500-lv),(i+25, 500-v))
+                                lv = v
+        else:
+                img = font.render("Mindwave Headset is not sending data... Press F5 to autoconnect or F6 to disconnect.", False, redColor)
+                window.blit(img,(100,100))
+        
+        for event in pygame.event.get():
+                if event.type==QUIT:
+                        pygame.quit()
+                        sys.exit()
+                if event.type==KEYDOWN:
+                        if event.key== K_F5:
+                                p.write_serial("\xc2")
+                        elif event.key== K_F6:
+                                p.write_serial("\xc1")
+                        elif event.key==K_ESCAPE:
+                                socket.close()
+                                pygame.quit()
+                                sys.exit()
+                        elif event.key == K_F7:
+                                record_baseline = True
+                                p.start_raw_recording("baseline_raw.csv")
+                                p.start_esense_recording("baseline_esense.csv")
+                        elif event.key == K_F8:
+                                record_baseline = False
+                                p.stop_esense_recording()
+                                p.stop_raw_recording()
+        pygame.display.update()
+        fpsClock.tick(30)
